d1a7dbfcdaa0e3e28b8ed64be389bb3f8937a43b
Faiza RABHI <143000774+faizawinWin@users.noreply.github.com>
2023-09-28 13:00:01 +0100

    feat : add section Hero

--- my-angular-project/src/app/hero/hero.component.ts
+++ my-angular-project/src/app/hero/hero.component.ts
@@ -9,5 +9,15 @@ export class HeroComponent {
   imageSrc1: string = '/assets/elements/1.png';
   imageSrc2: string = '/assets/elements/2.png';
   imageSrc3: string = '/assets/elements/3.png';
+  imageSrc23: string = '/assets/elements/23.jpg';
+  imageSrc4: string = '/assets/elements/4.png';
+  imageSrc14: string = '/assets/elements/14.jpg';
+  imageSrc16: string = '/assets/elements/16.jpg';
+  imageSrc17: string = '/assets/elements/17.jpg';
+  imageSrc18: string = '/assets/elements/18.jpg';
+  imageSrc19: string = '/assets/elements/19.jpg';
+  imageSrc10: string = '/assets/elements/10.png';
+  imageSrc25: string = '/assets/elements/25.png';
+  imageSrc24: string = '/assets/elements/24.png';
   imageAlt: string = 'My Image Alt Text';
 }
